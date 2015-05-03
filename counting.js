03118023e5991898bf88d072893214ffc2eb7797
Callie Brooks <calidesigns42@gmail.com>
2015-05-02 22:06:44 -0500

    Update counting.js
    My code for the counting game.  You can use the buttons to run the different programs.

--- counting.js
+++ counting.js
@@ -1,32 +1,79 @@
-// function where you have players counting
-// user picks number of players and the number counted to
-// used javascript instead of ruby
-var j = 0;
-var n = 1;
-var output = document.getElementById("result");
 
+// uses 10 players to count to 100
 var counter = function() {
-	var j = 0;
-	var n = 0;
+		var j = 1;
+		var text = "";
 		while (j < 101) {
 				for (i = 1; i < 11; i++) {
-					if ( j % 11 === 0 ) { //checks to see if number is divisble by 11
-						document.write("Player " + i + " says " + n + ". " + "<br/>");
-						i++;
-					}
-					else {
-						document.write("Player " + i + " says " + n + ". " + "<br/>");	
-					}
-					j++;
-					n = j;
+					  text += "Player " + i + " says " + j + ". " + "<br/>";
+						document.getElementById('result').innerHTML = text;
+				j++;
 				}
 		}
-		//output.innerHTML = "Player " + i +" says the last number: " +n+ ".";
+		
 };
 
-/*var reset = function() {
-		output.innerHTML = " ";
+// resets div to empty.
+var reset = function() {
+		document.getElementById('result').innerHTML = " ";
+};
 
-};*/
+// function skips player after a number is divisible by 11 is called.
+var Skip = function() {
+		var j = 1;
+	
+		var text = "";
+		while (j < 101) {
+				for (i = 1; i < 11; i++) {
+						if ( j % 11 === 0) {  // checks divisibility by 11
+							text += "Player " + i + " says " + j + ". " + "<br/>";
+							document.getElementById('result').innerHTML = text;
+							i++;
+						}
+						else {
+					  text += "Player " + i + " says " + j + ". " + "<br/>";
+						document.getElementById('result').innerHTML = text;
+						}
+				j++;
+			
+				}
+		}	
+};
 
-counter();
+// takes user input to set player# and number counted to.
+var cntr = function(player, number) {
+		var j = 1;
+		var text = "";
+		while (j <= number) {
+				for (i = 1; i <= player; i++) {
+					  text += "Player " + i + " says " + j + ". " + "<br/>";
+						document.getElementById('result').innerHTML = text;
+				j++;
+				}
+		}	
+};
+
+//switches directions when a number is divisible by 7
+var reverse = function() {
+		var j = 1;
+		var text = "";
+		var forward = true;
+		
+				var i = 1;
+				while (j < 101 && i < 11) {
+					if ( j % 7 === 0 ) {
+							forward =  !forward;	
+					}
+					  text += "Player " + i + " says " + j + ". " + "<br/>";
+						document.getElementById('result').innerHTML = text;
+				j++;
+				if (forward === true) {
+					i++;	
+				}
+				else {
+					i--;		
+				}
+				}
+		
+		
+};
