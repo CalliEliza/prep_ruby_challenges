5579065222853845319b2742f3f25d1e4e2c4ac6
Callie Brooks <calidesigns42@gmail.com>
2015-05-02 22:55:41 -0500

    Update challenges.js
    added power function

--- challenges.js
+++ challenges.js
@@ -1,8 +1,10 @@
 // 1, power
 var power = function (base, exponent) {
+  var result = base;
   for (i = 1; i <= exponent; i++) {
-    
+    result = result * base;
   }
+  return result;
 }
 
 // 2. factorial
