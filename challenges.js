fbb826344b4f3098aa0fcc664206f8fea1dbac0e
Callie Brooks <calidesigns42@gmail.com>
2015-05-02 22:51:49 -0500

    Update challenges.js
    completing other challenges for prep work.

--- challenges.js
+++ challenges.js
@@ -1,4 +1,14 @@
-// power
-var power = function () {
-
+// 1, power
+var power = function (base, exponent) {
+  for (i = 1; i <= exponent; i++) {
+    
+  }
 }
+
+// 2. factorial
+
+
+
+
+
+// 3. Uniques
